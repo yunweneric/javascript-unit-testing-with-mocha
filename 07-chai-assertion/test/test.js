0cf3f95789846aef352133698637fe0f127807fd
yunweneric <yunweneric@gmail.com>
2020-10-07 09:18:02 +0100

    added test

--- 07-chai-assertion/test/test.js
+++ 07-chai-assertion/test/test.js
@@ -2,29 +2,56 @@
 var assert = require('assert');
 var should = require('chai').should();
 
-describe('Object Test', function(){
-  it('should have property name', function(){
-    var car = {name:'Figo', Maker:'Ford'}
+describe('Object Test', function () {
+  it('should have property name', function () {
+    var car = { name: 'Figo', Maker: 'Ford' }
 
     car.should.have.property('name');
   });
 
-  it('should have property name with value Figo', function(){
-    var car = {name:'Figo', Maker:'Ford'}
+  it('should have property name with value Figo', function () {
+    var car = { name: 'Figo', Maker: 'Ford' }
     car.should.have.property('name').equal('Figo');
   });
 
-  it('should compare objects', function(){
-    var car = {name:'Figo', Maker:'Ford'}
-    var car1 = {name:'Figo', Maker:'Ford'}
+  it('should compare objects', function () {
+    var car = { name: 'Figo', Maker: 'Ford' }
+    var car1 = { name: 'Figo', Maker: 'Ford' }
 
     // car.should.equal(car1);
     car.should.deep.equal(car1);
   });
 
-  it('handling null', function(){
+  it('handling null', function () {
+    var car = null;
+    //car.should.not.exist;
+    should.not.exist(car);
+  });
+}); 
+describe('Object Test', function () {
+  it.skip('should have property name', function () {
+    var car = { name: 'Figo', Maker: 'Ford' }
+
+    car.should.have.property('name');
+  });
+
+  it('should have property name with value Figo', function () {
+    var car = { name: 'Figo', Maker: 'Ford' }
+    car.should.have.property('name').equal('Figo');
+  });
+
+  it('should compare objects', function () {
+    var car = { name: 'Figo', Maker: 'Ford' }
+    var car1 = { name: 'Figo', Maker: 'Ford' }
+
+    // car.should.equal(car1);
+    car.should.deep.equal(car1);
+  });
+
+  it('handling null', function () {
     var car = null;
     //car.should.not.exist;
     should.not.exist(car);
   });
 });
+
